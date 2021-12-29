9f9a99a5ba5e42c3a3dd00aa30d851c295db30f3
sumandeepb <sumandeep.banerjee@gmail.com>
2021-12-29 09:47:53 +0530

    update: convert server to class

--- src/srvcli/src/server.cpp
+++ src/srvcli/src/server.cpp
@@ -1,28 +1,37 @@
+#include <memory>
+
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/srv/add_two_ints.hpp"
 
-#include <memory>
+using std::placeholders::_1;
+using std::placeholders::_2;
 
-void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-          std::shared_ptr<example_interfaces::srv::AddTwoInts::Response>      response)
+class MinimalServer : public rclcpp::Node
 {
-  response->sum = request->a + request->b;
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
-                request->a, request->b);
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
-}
+public:
+    MinimalServer() : Node("add_two_ints_server")
+    {
+        service_ = this->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", std::bind(&MinimalServer::add, this, _1, _2));
+        RCLCPP_INFO(this->get_logger(), "Ready to add two ints.");
+    }
+
+private:
+    void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+             std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
+    {
+        response->sum = request->a + request->b;
+        RCLCPP_INFO(this->get_logger(), "Incoming request\na: %ld b: %ld", request->a, request->b);
+        RCLCPP_INFO(this->get_logger(), "sending back response: [%ld]", (long int)response->sum);
+    }
+
+    rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_;
+};
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalServer>());
+    rclcpp::shutdown();
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
-
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-    node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
-
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
-
-  rclcpp::spin(node);
-  rclcpp::shutdown();
+    return 0;
 }
