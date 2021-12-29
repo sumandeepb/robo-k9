30562ee5cd1c1a677ebd34905702f666f812a359
sumandeepb <sumandeep.banerjee@gmail.com>
2021-12-29 18:21:09 +0530

    update: convert client to class

--- src/srvcli/src/client.cpp
+++ src/srvcli/src/client.cpp
@@ -1,47 +1,68 @@
-#include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/srv/add_two_ints.hpp"
-
 #include <chrono>
 #include <cstdlib>
 #include <memory>
 
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
+
 using namespace std::chrono_literals;
+
+class MinimalClient : public rclcpp::Node
+{
+public:
+    MinimalClient() : Node("add_two_ints_client")
+    {
+        client_ = this->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+    }
+
+    int spin_custom(int argc, char **argv)
+    {
+        if (argc != 3)
+        {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: add_two_ints_client X Y");
+            return 1;
+        }
+
+        auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+        request->a = atoll(argv[1]);
+        request->b = atoll(argv[2]);
+
+        while (!client_->wait_for_service(1s))
+        {
+            if (!rclcpp::ok())
+            {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+                return 0;
+            }
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+        }
+
+        auto result = client_->async_send_request(request);
+        // Wait for the result.
+        if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == rclcpp::FutureReturnCode::SUCCESS)
+        {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
+        }
+        else
+        {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+        }
+
+        return 0;
+    }
+
+private:
+    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client_;
+};
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);
 
-  if (argc != 3) {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: add_two_ints_client X Y");
-      return 1;
-  }
+    MinimalClient client_obj;
+    client_obj.spin_custom(argc, argv);
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
-  rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
-    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+    rclcpp::shutdown();
 
-  auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-  request->a = atoll(argv[1]);
-  request->b = atoll(argv[2]);
-
-  while (!client->wait_for_service(1s)) {
-    if (!rclcpp::ok()) {
-      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
-      return 0;
-    }
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
-  }
-
-  auto result = client->async_send_request(request);
-  // Wait for the result.
-  if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::FutureReturnCode::SUCCESS)
-  {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
-  } else {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
-  }
-
-  rclcpp::shutdown();
-  return 0;
+    return 0;
 }
