9df1a7017f980659dc65a06785c2597469166b6f
sumandeepb <sumandeep.banerjee@gmail.com>
2023-05-03 21:02:53 +0530

    update: UNO+Adafruit Motor Driver example

--- arduino/tutorials/motor/motor.ino
+++ arduino/tutorials/motor/motor.ino
@@ -7,16 +7,18 @@ void setup() {
   // put your setup code here, to run once:
   motor_left.setSpeed(208);
   motor_right.setSpeed(208);
+  motor_left.run(FORWARD);
+  motor_right.run(FORWARD);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  motor_left.run(FORWARD);
+  /*motor_left.run(FORWARD);
   delay(2000);
   motor_left.run(RELEASE);
   delay(1000);
   motor_right.run(FORWARD);
   delay(2000);
   motor_right.run(RELEASE);
-  delay(1000);
+  delay(1000);*/
 }
