485bc8719591285a61dd3e1e503429f05bc94a5c
sumandeepb <sumandeep.banerjee@gmail.com>
2023-05-06 00:45:40 +0530

    update: obstacle avoidance logic

--- arduino/panzer-XI/panzer-XI.ino
+++ arduino/panzer-XI/panzer-XI.ino
@@ -8,13 +8,45 @@ const int WALL_SENSOR_RIGHT = A4;
 
 AF_DCMotor motor_left(3);
 AF_DCMotor motor_right(4);
+
+unsigned long leftWallTime = 0;
+unsigned long rightWallTime = 0;
+const unsigned long PULSE_DURATION = 500;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(WALL_SENSOR_LEFT, INPUT);
+  pinMode(WALL_SENSOR_RIGHT, INPUT);
+
   motor_left.setSpeed(208);
-  motor_right.setSpeed(208);
+  motor_right.setSpeed(208);        
+  motor_left.run(FORWARD);
+  motor_right.run(FORWARD);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  unsigned int currentTime = millis();
+  bool leftWallSensor = digitalRead(WALL_SENSOR_LEFT);
+  bool rightWallSensor = digitalRead(WALL_SENSOR_RIGHT);
+
+  if(LOW == leftWallSensor) { // active low
+    leftWallTime = currentTime;
+  }
+
+  if(LOW == rightWallSensor) { // active low
+    rightWallTime = currentTime;
+  }
+
+  if (currentTime - leftWallTime < PULSE_DURATION) {
+      motor_right.run(BACKWARD);
+  } else {
+      motor_right.run(FORWARD);
+  }
+
+  if (currentTime - rightWallTime < PULSE_DURATION) {
+      motor_left.run(BACKWARD);
+  } else {
+      motor_left.run(FORWARD);
+  }
 }
